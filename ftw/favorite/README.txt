e7ea448ff4b5bd34472c2bb374d9030cf42f906d
Timon Tschanz <timon.tschanz@4teamwork.ch>
2011-12-01 15:56:29 +0100

    cleanup for github

--- ftw/favorite/README.txt
+++
@@ -1,4 +0,0 @@
-Introduction
-============
-
-A favorite like content type, as we had in plone 3
--- setup.py
+++ setup.py
@@ -3,60 +3,53 @@
 This module contains the tool of ftw.favorite
 """
 from setuptools import setup, find_packages
+import os
 
-def read(*rnames):
-    return open('/'.join(rnames)).read()
 
-version = open('ftw/favorite/version.txt').read().strip()
+version = '1.1.1dev'
 maintainer = 'Mathias Leimgruber'
-
-long_description = (
-    read('README.txt')
-    + '\n\n\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('docs', 'HISTORY.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('ftw', 'favorite', 'README.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
 
 tests_require=['zope.testing']
 
 setup(name='ftw.favorite',
       version=version,
-      description="ftw.favorite (Maintainer: %s)" % maintainer,
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description="ftw.favorite provides content-type like favorites",
+      long_description = open('README.rst').read() + '\n' + \
+            open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw favorite plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/ftw.favorite',
+      url='https://github.com/4teamwork/ftw.favorite',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=['setuptools',
                         # -*- Extra requirements: -*-
                         ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
       test_suite = 'ftw.favorite.tests.test_docs.test_suite',
       entry_points="""
-      # -*- entry_points -*- 
+      # -*- entry_points -*-
       """,
       )
